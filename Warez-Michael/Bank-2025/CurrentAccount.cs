522de28fff7d8805dd5886676fe2e59670b717ee
miharadeskaa <sillimika@hotmail.com>
2025-10-11 12:45:33 +0200

    new

---
+++ Warez-Michael/Bank-2025/CurrentAccount.cs
@@ -0,0 +1,15 @@
+/*
+creer une classe "CurrentAccount" qui permet la gestion d' un compte
+propirété publique : Nombre,double balance(lecture seule), CreditLine ,Owner
+methodes publiques : void Withdraw, void deposit(double amount)
+*/
+
+class CurrentAccount
+
+{
+    public int Nombre { get; set; }
+    public double Balance { get; }
+
+    
+
+}
---
+++ Warez-Michael/Bank-2025/personnes.cs
@@ -0,0 +1,16 @@
+/*
+Créer une classe "Person" implémentant : 
+les propiétés publiques : Nom,Prénom,Anniversaire
+*/
+
+
+
+class Person
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public DateTime BirthDate { get; set; }
+}
+
