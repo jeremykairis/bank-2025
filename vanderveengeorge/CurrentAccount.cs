901cd0929340c56f8377222a6545a5be51f73c12
gg <georgevanderveen@yahoo.fr>
2025-10-12 21:52:36 +0200

    Implement Withdraw and Deposit methods to CurrentAccount

--- vanderveengeorge/CurrentAccount.cs
+++ vanderveengeorge/CurrentAccount.cs
@@ -1,16 +1,23 @@
 class CurrentAccount(string number, double balance, double creditLine, Person owner)
 {
     public string Number { get; set; } = number;
-    public double Balance { get; } = balance;
+    public double Balance { get; private set; } = balance;
     public double CreditLine { get; set; } = creditLine;
     public Person Owner { get; set; } = owner;
 
     public void Withdraw(double amount)
     {
-
+        if (amount > Balance)
+        {
+            Console.WriteLine("Solde insufisant");
+        }
+        else
+        {
+            Balance -= amount;
+        }
     }
     public void Deposit(double amount)
     {
-
+        Balance += amount;
     }
 }
--- vanderveengeorge/Program.cs
+++ vanderveengeorge/Program.cs
@@ -5,5 +5,10 @@ class Program
     {
         // Création d'une instance de la classe Person
         Person person1 = new Person("Alice", "Dupont", new DateTime(1990, 5, 21));
+        CurrentAccount compte1 = new CurrentAccount("BE1234-4323-4323-3424", 4553, 423, person1);
+        Console.WriteLine(compte1.Balance);
+        compte1.CreditLine -= 3;
+        compte1.Withdraw(300);
+        Console.WriteLine(compte1.Balance);
     }
 }
