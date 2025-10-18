eb088362e8ff8cf6bfe4ae1237116641e20fa218
Mertens Adrien <mertensadrien23@hotmail.be>
2025-10-18 23:32:55 +0200

    Add `CurrentAccount` class definition with basic properties and methods

--- adrienMertens/program.cs
+++ adrienMertens/program.cs
@@ -4,3 +4,22 @@
     private string LastName;
     private DateTime BirthDate;
 }
+
+class CurrentAccount
+{
+    private string number;
+    private double readonly balance;
+    private double creditLine;
+    private Person owner;
+
+    public void WithDraw(double amount)
+    {
+        
+    }
+    public void Deposit(double amount)
+    {
+        
+    }
+}
+
+
