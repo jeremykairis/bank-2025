6bb4ecc8440bab72bfc0688ee501903b5cf1b90f
Lion <damien.crappe13@outlook.com>
2025-10-18 09:53:49 +0200

    2-3 corrections

--- damien crappe/bank/Program.cs
+++ damien crappe/bank/Program.cs
@@ -7,33 +7,67 @@ class Person
     public string FirstName;
     public string LastName;
     public DateTime BirthDate;
+    public Person(string firstName, string lastName, DateTime birthDate)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        BirthDate = birthDate;
+    }
 }
 class CurrentAccount
 {
-    public string Number;
+    public string Number { get; set; }
     public double Balance { get; }
-    public double CreditLine;
-    public Person Owner;
+    public double CreditLine { get; set; }
+    public Person Owner { get; set; }
+    public CurrentAccount(string number, double balance, double creditLine, Person owner)
+    {
+        Number = number;
+        Balance = balance;
+        CreditLine = creditLine;
+        Owner = owner;
+    }
     public void Deposit(double amount, double Balance)
     {
         Balance += amount;
+        return;
     }
     public void Withdraw(double amount, double Balance)
     {
-        Balance -= amount;
+        if (Balance <= 0)
+        {
+            Console.WriteLine("Le solde est insuffisant pour effectuer ce retrait.");
+            return;
+        }
+        else if (Balance - amount < 0)
+        {
+            Console.WriteLine("Retrait impossible, le compte serait à découvert.");
+            return;
+        }
+        else
+        {
+            Balance -= amount;
+            return;
+        }
     }
 }
 class Bank
 {
-    public readonly Dictionary<string, CurrentAccount> Accounts = [];
+    public readonly Dictionary<string, CurrentAccount> Accounts = new ();
     public string Name;
+    public Bank(string name)
+    {
+        Name = name;
+    }
     public void AddAccount(CurrentAccount account)
     {
         Accounts[account.Number] = account;
+        return;
     }
     public void RemoveAccount(string accountNumber)
     {
         Accounts.Remove(accountNumber);
+        return;
     }
 
 }
@@ -54,5 +88,6 @@ class Methods
             }
         }
         Console.WriteLine($"La somme des comptes de {owner.FirstName} {owner.LastName} est de {SumAccount} euros.");
+        return;
     }
 }
